90d79615b9ce7ecf812133869f34cab06b137494
Jeremiah Rose <jeremiah.aaron.rose@gmail.com>
2018-07-18 23:39:52 +1000

    Minimum font size, README introduction

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # perfectFit.js
-Make headings fit their divs perfectly
+Make headings fit their divs perfectly.
+
+Features:
+- Tiny. (2kb)
+- Fast. Runs once on page load, then lets CSS do the rest. No event handlers.
+- The ONLY text fitting library that properly scales wrapped text.
+- Optional minimum font size.
+- Works with custom fonts & fancy text styles with fully selectable text.
+- Tested on latest versions of Chromium and Firefox as of July 2018.
+
+Usage:
+<script src="perfectFit.js"></script>
+<div class="container">
+    <div id="heading">A Long Expected Party</div>
+</div>
+<script>
+    perfectFit("heading", "30px");
+</script>
+
+How it works:
+Behind the hood, perfectFit breaks your text into individual words and renders
+each word as inline SVG. These are then placed in a Flexbox container which
+responsively scales the words to snugly fit the edges of the containing div.
+
+Known limitations:
+* Requires a modern browser with Flexbox and SVG support. Not yet tested 
+on: Safari, Opera, Android or IE, or mobile browsers. I would appreciate some 
+help improving the compatibility and writing polyfills where necessary.
+
+* Still ironing out some issues with padding and spacing
+
+* Cannot do hyphenated word-breaks (yet).
+
+* If using webfonts, perfectFit must be run after the fonts are loaded. 
+To this end, it uses the document.fonts.ready.then() handler, which 
+currently works on Chrome but not in Firefox. For Firefox you will have to
+manually ensure that your fonts are loaded before running perfectFit().
--- perfectFit.js
+++ perfectFit.js
@@ -1,7 +1,8 @@
-function perfectFit(textId, minWordSize){
+function perfectFit(textId, minFontSize="1em"){
    document.fonts.ready.then(function () {
         var tempElem = document.getElementById(textId);
         var container = tempElem.parentElement;
+        console.log(minFontSize);
         Object.assign(tempElem.style, {
             visibility:"visible",
             display:"inline-block",
@@ -11,7 +12,8 @@ function perfectFit(textId, minWordSize){
             overflow:"visible",
             whiteSpace:"nowrap",
             padding:0,
-            margin: 0
+            margin: 0,
+            fontSize: minFontSize
         });
         var text = tempElem.innerHTML; 
         var words = text.split(/\s+/);
@@ -24,23 +26,20 @@ function perfectFit(textId, minWordSize){
         var output = "";
         for (var word of words){
             var width, height;
+            console.log("width");
             [width, height] = textWidthHeight(word);
             output += svgWord(word, width, height, width/totalWidth * 100);
         }
         container.innerHTML = "<div style='display:flex; flex-wrap:wrap;'>" + output + "</div>";
         
-        function svgWord(text, width, height, percentage){/*
-            var debug = tempElem.cloneNode(true);
-            document.body.appendChild(debug);
-            debug.innerHTML = text;*/
-            return "<svg style='margin: 0 " + Math.round(spacing * 2)
-                           + "; min-width:" + Math.round(minWordSize * width)
-                           + "px; flex-basis:"+ Math.round(width)
-                           + "px; flex-grow:" + Math.round(percentage)
-                           /*+ "; width:" + Math.round(percentage)*/
-                           + "; height:100%;" 
-                           + "' viewBox='0 0 " + Math.round(width) + " " + Math.round(height) + "'>"
-                + "<text id='"+textId+"' x='0' y='0' dominant-baseline='text-before-edge'>" + text 
+        function svgWord(text, width, height, percentage){
+            return "<svg style='padding: 0px " + spacing * 2 + "px; "
+                             + "min-width:" + width + "px; "
+                             + "flex-basis:"+ width + "px; "
+                             + "flex-grow:" + Math.round(percentage) + "; "
+                             + "height:100%;" + "' "
+                             + "viewBox='0 0 " + Math.round(width) + " " + Math.round(height) + "'>"
+                + "<text id='"+textId+"' x='0' y='0' font-size='"+minFontSize+"' dominant-baseline='text-before-edge'>" + text 
                 +"</text></svg>"
         }
 
